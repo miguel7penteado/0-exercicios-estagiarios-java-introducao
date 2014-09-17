e5630605e885892513e7ba38e93e105cf3b8d762
Miguel Suez Xve Penteado <miguel7penteado@gmail.com>
2014-09-17 02:07:16 -0300

    enunciado do exercicio1

---
+++ 2-CompilarNaMao/fontes/JuntaTudoEExecuta.java
@@ -0,0 +1,12 @@
+package meuprograma;
+
+import matematica.MinhaMultiplicacao;
+
+public class JuntaTudoEExecuta
+{
+	public static void main(String[] args)
+	{
+		MinhaMultiplicacao objeto1 = new MinhaMultiplicacao();
+		System.out.println("O resultado é: " + objeto1.multiplica(5, 10) );
+	}
+} 
---
+++ 2-CompilarNaMao/fontes/MinhaMultiplicacao.java
@@ -0,0 +1,9 @@
+package matematica;
+
+public class MinhaMultiplicacao
+{
+	public int multiplica(int parcela1, int parcela2)
+	{
+		return (parcela1 * parcela2);
+	}
+} 
